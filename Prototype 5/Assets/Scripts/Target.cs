434480bd5da9f807eaa65c1f76343f91564c510b
Mahima SIngh <mahima.ms0007@gmail.com>
2022-06-07 09:59:29 +0200

    added gameplay
    spawning a list of game object, OnMousDown() function to click on a gameobject and destroy it. adding torque and force to our game objects.

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody Rb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    
+    private float maxForce = 10;
+    private float xRange = 4;
+    private float yRange = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+
+        Rb.AddForce(RandomForce(), ForceMode.Impulse);
+        Rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); //for rotation
+
+        transform.position = newPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxForce, maxForce);
+    }
+    Vector3 newPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), -yRange);
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
